d5b6d805ac88e4893a8b70a7483d08b274cb42f9
mehdi.kerbedj1 <mehdi.kerbedj@apalia.net>
2022-11-23 10:42:05 +0100

    Deploy vpc with  mehdi-wsc module

---
+++ input.tf
@@ -0,0 +1,24 @@
+variable "group" {
+  default = ""
+}
+variable "env" {
+  default = ""
+}
+variable "region" {
+  default = ""
+}
+variable "vpc_cidr" {
+  default = ""
+}
+variable "public_subnet_count" {
+  default = ""
+}
+variable "private_subnet_count" {
+  default = ""
+}
+variable "cidr_block_private" {
+  default = ""
+}
+variable "cidr_block_public" {
+  default = ""
+}
---
+++ terraform.tfvars
@@ -0,0 +1,8 @@
+group                = "apalia"
+env                  = "dev"
+region               = "us-east-2"
+vpc_cidr             = "15.0.0.0/16"
+public_subnet_count  = "1"
+private_subnet_count = "1"
+cidr_block_private   = ["15.0.1.0/24"]
+cidr_block_public    = ["15.0.2.0/24"]
